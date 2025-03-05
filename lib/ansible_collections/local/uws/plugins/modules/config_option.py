5319709837b10c98b46b2a8d41f511fcde221992
reksarka <reksarka@gmail.com>
2025-03-05 14:54:22 +0200

    [TEST] ansible_collections
    Plugin: local.uws.config_option
    
    Test for removing an option when no value is provided.
    
    Needs the feature implementation.

--- lib/ansible_collections/local/uws/plugins/modules/config_option.py
+++ lib/ansible_collections/local/uws/plugins/modules/config_option.py
@@ -7,35 +7,61 @@ __metaclass__ = type
 
 DOCUMENTATION = r'''
 ---
-module: unique_option
-
-short_description: Ensure a config option is unique within the config file.
-
+module: config_option
+short_description:
+    Ensure a config option is unique within the config file.
 description:
     Like the "ansible.builtin.lineinfile" module, but ensures that only one
     option with the specified name is exists. If the option is already exists,
     it will be rewritten. But, if there are several options are exists, they
     will be deleted. This is the difference from the
     "ansible.builtin.lineinfile", that can only rewrite the first or last line.
-
 options:
     file:
-        description: A config file.
+        description: A config file of INI format.
+        type: path
         required: true
-        type: str
-    line:
+    option:
         description:
-            Option line "<option>=<value>", where <option> must be unique
-            within the file.
-        required: true
+            Option name. Will be the unique within the file. All existing
+            options with the specified name will be removed before adding a new
+            entry.
         type: str
+        required: true
+    value:
+        description:
+            Option value. Will be ignored when state is "absent". When empty
+            and the state is "present", the "<option>=" line will be added
+            (see EXAMPLES).
+        type: str
+    state:
+        description:
+            Add or remove the option.
+        type: str
+        choices: [present, absent]
+        default: present
 '''
 
 EXAMPLES = r'''
-- name: Add bash alias `ll`
-  unique_option:
+- name: Remove all entries of `ll` alias
+  local.uws.config_option:
     file: ~/.bash_aliases
-    line: alias ll='ls -l --color=auto'
+    option: alias ll
+    state: absent
+
+# Instead of previous example, the state is "present" by default and the
+# "alias ll=" will be added to ~/.bash_aliases instead of removing the alias.
+- name: Add alias with the empty value
+  local.uws.config_option:
+    file: ~/.bash_aliases
+    option: alias ll
+
+# The "alias ll='ls -l --color=auto'" line will be added.
+- name: Add bash alias `ll`
+  local.uws.config_option:
+    file: ~/.bash_aliases
+    option: alias ll
+    value: "'ls -l --color=auto'"
 '''
 
 RETURN = r'''#'''
