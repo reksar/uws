0e3885f71ad322d626a9a630de663c28f55ee1cd
reksarka <reksarka@gmail.com>
2025-07-21 04:01:20 +0300

    [UPD] tmux battery status
    Expects the third-party battery util, configured during harware setup.

--- lib/ansible_collections/local/uws/roles/system/templates/battery.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-BATTERY_PATH="{{ battery_path }}"
-capacity="$(cat $BATTERY_PATH/capacity)"
-[[ "$(cat $BATTERY_PATH/status)" == "Charging" ]] && charge="+" || charge=""
-echo "$capacity%$charge"
