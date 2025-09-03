58266dc3c58fc958def3e37f69d1c32b882bb535
reksarka <reksarka@gmail.com>
2025-09-04 00:00:24 +0300

    [UPD] uws set
    Allows the remote connection via SSH.
    
    Requires the Python on a remote host and `sshpass` for auth via SSH
    using the password instead of SSH key.

--- util/uws/set.sh
+++ util/uws/set.sh
@@ -6,19 +6,49 @@ uws="${uws:-$(cd "$(dirname $BASH_SOURCE[0])/../.." && pwd)}"
 . "$uws/lib/uws/set.sh"
 
 
-arg="${1:-main}"
+# -- Ansible playbook and target {{{
 
-playbook="$(playbook_file "$arg")"
-target="$(playbook_target "$arg")"
+ansible_target="${1:-main}"
+
+playbook="$(playbook_file "$ansible_target")"
+target="$(playbook_target "$ansible_target")"
 
 [[ -z "$playbook" ]] && {
-  ERR "Playbook is not found: '$arg'!"
+  ERR "Playbook is not found: '$ansible_target'!"
   exit 1
 }
 
 INFO "Playbook: '$playbook'."
 
 [[ -n "$target" ]] && INFO "Target: '$target'."
+
+# -- Ansible playbook and target }}}
+
+
+# -- Connection {{{
+
+target_host="${2:-}"
+
+if [[ -z $target_host ]]
+then
+  connection="--connection=local --inventory=localhost,"
+else
+
+  [[ "$target_host" == *@* ]] && user="${target_host%%@*}" || user=""
+
+  [[ -z $user ]] && {
+    ERR "No user for remote connection!"
+    exit 1
+  }
+
+  host="${target_host##*@}"
+  auth="${3:---ask-pass}"
+
+  connection="--connection=ssh --inventory=$host, --user=$user $auth"
+
+fi
+
+# -- Connection }}}
 
 
 # -- Extra vars {{{
@@ -38,9 +68,9 @@ extra_vars="uws='$uws' $playbook_target $xdg_vars"
 # -- Extra vars }}}
 
 
+# FIXME: extra vars are not supported for remote host!
 ensure_ansible && ansible-playbook \
-  --inventory=localhost, \
-  --connection=local \
+  $connection \
   --ask-become-pass \
   --extra-vars="$extra_vars" \
   "$playbook"
